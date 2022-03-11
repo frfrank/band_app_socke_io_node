03186cd55926ffccd7abd1a919739ee48ad65bc4
Francisco Picado <franciscopicado19@gmail.com>
2022-03-11 21:48:10 +0100

    ✅  send band for app

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,9 +1,23 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
 
+const bands = new Bands();
+
+bands.addBand( new Band('Queen'));
+bands.addBand( new Band('Bon Jovi'));
+bands.addBand( new Band('Metálica'));
+bands.addBand( new Band('Héroes del silencio'));
+bands.addBand( new Band('Beatles'));
+
+
+console.log(bands);
 
 // Mensajes de Sockets
 io.on('connection', client => {
     console.log('Cliente conectado');
+
+    client.emit('active-bands', bands.getBands());
 
     client.on('disconnect', () => {
         console.log('Cliente desconectado');
@@ -22,6 +36,12 @@ io.on('connection', client => {
         io.emit( 'saludo', { lucas: 'Hola que hace' } );
 
     });
+
+    client.on('emitir-mensaje', payload => {
+        console.log("Mensaje Recibido", payload);
+        client.broadcast.emit("emitir-mensaje", payload);
+        client.broadcast.emit("nuevo-mensaje", payload);
+    })
 
 
 
